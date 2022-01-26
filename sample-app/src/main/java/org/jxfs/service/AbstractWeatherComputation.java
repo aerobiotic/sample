f36b8114e1f913b228be4540e41a8facb31a774b
Jay P. Drummond <jay.drummond.jd@gmail.com>
2022-01-26 11:20:30 -0500

    refactor WeatherEvent

--- sample-app/src/main/java/org/jxfs/service/AbstractWeatherComputation.java
+++ sample-app/src/main/java/org/jxfs/service/AbstractWeatherComputation.java
@@ -5,18 +5,17 @@ import org.jxfs.events.WeatherEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractWeatherComputation {
-	
+
 //	@Autowired
 //    private WeatherObservationRepository weatherObservationRepository;
 
 	protected WeatherEvent computeLastWeatherEvent(String location) {
-		// TODO look up observations in the DB based on location and last N hours. 
+		// TODO look up observations in the DB based on location and last N hours.
 		// TODO use AI to determine the type and severity of the weather
-		WeatherEvent weatherEvent = WeatherEvent.THUNDERSTORM;
-		weatherEvent.setSeverity(WeatherEvent.SEVERE);
-		weatherEvent.setLocation(location);
+		WeatherEvent weatherEvent = new WeatherEvent(WeatherEvent.WeatherType.THUNDERSTORM,
+				WeatherEvent.WeatherSeverity.SEVERE, location);
 		return weatherEvent;
-		
+
 	}
 
 }
